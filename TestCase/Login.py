787bc4d409348e005707a631ab75be755a8fb7c0
dykim <dykim@suprema.co.kr>
2023-02-21 16:56:05 +0900

    go commit

--- TestCase/Login.py
+++ TestCase/Login.py
@@ -1,3 +1,6 @@
+from lib2to3.pgen2 import driver
+from unittest import TestCase
+
 from mobileTest import BeforeTest
 from selenium.webdriver.common.by import By
 from appium.webdriver.common.mobileby import AppiumBy
@@ -19,17 +22,20 @@ import time, datetime
 
 wd = BeforeTest.Apprun()
 
-wd.find_element(AppiumBy.XPATH, '//android.widget.Button[@content-desc="로그인"]').click()
+wd.find_element(AppiumBy.XPATH,
+                '//android.widget.Button[@content-desc="로그인"]').click()
 print('Login View 진입')
 
-LoginID = wd.find_element(AppiumBy.XPATH, '//android.widget.EditText[@text="put phone number"]')
+LoginID = wd.find_element(AppiumBy.XPATH,
+                          '//android.widget.EditText[1]')
 LoginID.click()
 time.sleep(2)
-LoginID.send_keys('01090497847')
+LoginID.send_keys('01055559999')
 time.sleep(2)
 print('휴대폰 번호 입력 완료')
 
-LoginPassword = wd.find_element(AppiumBy.XPATH, '//android.widget.EditText[@text="put password"]')
+LoginPassword = wd.find_element(AppiumBy.XPATH,
+                                '//android.widget.EditText[2]')
 time.sleep(2)
 LoginPassword.click()
 time.sleep(2)
@@ -37,8 +43,13 @@ LoginPassword.send_keys('Rlaehdus100!')
 print('Password 입력 완료')
 print('Login Test : Pass')
 
-LoginButton = wd.find_element(AppiumBy.ACCESSIBILITY_ID, 'Login')
+LoginButton = wd.find_element(AppiumBy.ACCESSIBILITY_ID, '로그인')
 LoginButton.click()
+
+# Mobile 의 Notification 창을 열어준다
+# wd.open_notifications()
+
+
 
 
 
